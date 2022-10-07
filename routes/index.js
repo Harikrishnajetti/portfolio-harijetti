649557be3b9379c780f862e6050a4db7010c3e1b
harijetti <jettiharikrishna@gmail.com>
2022-10-07 14:33:50 -0400

    first

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,15 @@
 var express = require('express');
 var router = express.Router();
+// <!-- <header class="header">
+//         <div class="container">
+//                 <p class="text-muted">
+//                 File Name: <a>index.js</a><br/>
+//                 Student Name: <a>Hari Krishna Jetti</a><br/>
+//                 Student Id: <a>j301241201</a><br/>
+//                 Date: <a>07-October-2022</a><br/>
+//                 </p>
+//         </div>
+//       </header> -->
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,26 +18,26 @@ router.get('/', function(req, res, next) {
 
 /* GET home page. */
 router.get('/home', function(req, res, next) {
-  res.render('index', { title: 'Home' });
+  res.render('home', { title: 'Home' });
 });
 
 /* GET About us page. */
 router.get('/about', function(req, res, next) {
-  res.render('index', { title: 'About Us' });
+  res.render('about', { title: 'About Me' });
 });
 
-/* GET Products page. */
-router.get('/products', function(req, res, next) {
-  res.render('index', { title: 'Products' });
+/* GET Projects page. */
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'Projects' });
 });
 
 /* GET Services page. */
 router.get('/services', function(req, res, next) {
-  res.render('index', { title: 'Services' });
+  res.render('services', { title: 'Services' });
 });
 
 /* GET Contact us page. */
 router.get('/contact', function(req, res, next) {
-  res.render('index', { title: 'Contact us' });
+  res.render('contact', { title: 'Contact Me' });
 });
 module.exports = router;
